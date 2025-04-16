f1bb90c5d86308aec02e1c6c2366b7fc798abfb5
Andrii D. <and05gt@gmail.com>
2025-04-16 23:20:40 +0200

    Added redux/auth

---
+++ src/redux/auth/operations.js
@@ -0,0 +1,61 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const petloveApi = axios.create({
+  baseURL: "https://petlove.b.goit.study/api",
+});
+
+const setAuthHeader = (token) => {
+  petloveApi.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await petloveApi.post("/users/signup", credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await petloveApi.post("/users/signin", credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logout = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
+  try {
+    await petloveApi.post("/users/signout");
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const refreshUser = createAsyncThunk(
+  "auth/refreshUser",
+  async (_, thunkAPI) => {
+    const savedToken = thunkAPI.getState().auth.token;
+    if (!savedToken) {
+      return thunkAPI.rejectWithValue("No valid token");
+    }
+    setAuthHeader(savedToken);
+    try {
+      const { data } = await petloveApi.get("/users/current");
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
